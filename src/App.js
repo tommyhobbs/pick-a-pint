a65ca1887b1f7c0541951903d1f616c82519644c
Tom Hobbs <tomhobbs@Toms-MBP.lan>
2018-07-29 12:49:15 +0100

    simplification

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Helmet from 'react-helmet';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,13 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Helmet title="Pick a Pint" />
-        <div className="App-header">
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Pick a Pint <span role="img" aria-label="pint emoji">🍺</span></h2>
-        </div>
+        </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <h2>Pick a Pint <span role="img"  className="App-logo" aria-label="pint emoji">🍺</span></h2>
         </p>
       </div>
     );
