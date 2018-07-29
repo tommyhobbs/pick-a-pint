d3b7b3bc5c3508174079593f2b34633686d4fed1
Tom Hobbs <tomhobbs@Toms-MBP.lan>
2018-07-29 19:07:55 +0100

    styling and footer links.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
-import logo from './logo.svg';
+import linkedIn from './LinkedIn-66px.png';
+import gitHub from './GitHub-64px.png';
 import './App.css';
 
 class App extends Component {
@@ -9,11 +10,24 @@ class App extends Component {
       <div className="App">
         <Helmet title="Pick a Pint" />
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Pick a Pint <span role="img"  className="App-logo" aria-label="pint emoji">🍺</span></h2>
+          {/* <img src={logo} className="App-logo" alt="logo" />
+          <span role="img"  className="App-logo" aria-label="pint emoji">🍺</span> */}
+          <h1 className='App-title'>Pick a Pint!</h1>
         </div>
-        <p className="App-intro">
-        </p>
+        <div className="Content">
+          <p className="Intro">
+            Currently in development so please, watch this space!
+          </p> 
+        </div>
+        <div className="Footer">
+          <a href='https://www.linkedin.com/in/tomghobbs/' target="_blank" rel="noopener noreferrer">
+            <img src={linkedIn} className="Logo LinkedIn" alt="LinkedIn Logo" />
+          </a>
+          <a href='https://github.com/tommyhobbs/pick-a-pint' target="_blank" rel="noopener noreferrer">
+            <img src={gitHub} className="Logo GitHub" alt="GitHub Logo" />
+          </a>
+          <p>a silly idea by Tom Hobbs </p>
+        </div>
       </div>
     );
   }
