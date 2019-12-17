eda5c3bf54fe3d615fa636b27f661b0a00a1326d
Tom Hobbs <thomas.hobbs@bbc.com>
2019-12-17 14:21:02 +0000

    Working transform skew + mirror

--- src/components/pintBackground/PintBackground.jsx
+++ src/components/pintBackground/PintBackground.jsx
@@ -1,46 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import PropTypes from 'prop-types';
 // import beer from '../../assets/beer.png';
-import './pintBackground.css';
+import "./pintBackground.css";
 
 class PintBackground extends Component {
   render() {
-    const { gamma, width, height } = this.props
-    const fillLevel = (Math.abs(gamma) / 90) * height
-    const xCenter = width / 2 + ((gamma / 90) * width / 2)
-    console.log('width: ' + width)
-    console.log('fillLevel: ' + fillLevel)
-    const origin = `${xCenter}px ${fillLevel}px`
-    console.log('origin: ', origin)
+    const { gamma, width, height } = this.props;
+    const angle = Math.abs(gamma);
+    const xPos = gamma < 0 ? 0 : -width;
+    const flip = gamma < 0 ? 1 : -1;
     return (
-      <svg xmlns='http://www.w3.org/2000/svg' width={width} >
+      <svg xmlns="http://www.w3.org/2000/svg" width={width}>
         <rect
-          width={Math.sqrt((width * width) + (height * height))}
+          width={width}
           height={height}
-          style={
-            {
-              fill: '#F3CD57',
-              transform: `rotate(${-gamma}deg)`,
-              transformOrigin: origin
-            }
-          }
-          y={fillLevel}
+          style={{
+            fill: "#F3CD57",
+            transform: `scaleX(${flip}) translate(${xPos}px, ${0}px) skewY(${angle}deg)`
+          }}
         />
         <rect
-          width='10'
-          height='10'
-          style={
-            {
-              stroke: '#006600',
-              fill: '#00cc00'
-            }
-          }
-          x={xCenter}
-          y={fillLevel}
+          width="10"
+          height="10"
+          style={{
+            stroke: "#006600",
+            fill: "#00cc00"
+          }}
+          x={xPos}
+          y={0}
         />
       </svg>
     );
   }
-};
+}
 
 export default PintBackground;
