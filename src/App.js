c10196f23e49f2e4c9ef19ed90c482bcdb4486c8
Tom Hobbs <thomas.hobbs@bbc.com>
2019-12-17 16:11:50 +0000

    WIP pouring

--- src/App.js
+++ src/App.js
@@ -1,67 +1,107 @@
-import React, { Component } from 'react';
-import Helmet from 'react-helmet';
-import { renderToStaticMarkup } from 'react-dom/server'
-import linkedIn from './assets/LinkedIn-66px.png';
-import gitHub from './assets/GitHub-64px.png';
-import PintBackground from './components/pintBackground/PintBackground';
-import './App.css';
+import React, { Component } from "react";
+import Helmet from "react-helmet";
+import { renderToStaticMarkup } from "react-dom/server";
+import linkedIn from "./assets/LinkedIn-66px.png";
+import gitHub from "./assets/GitHub-64px.png";
+import PintBackground from "./components/pintBackground/PintBackground";
+import "./App.css";
 
 class App extends Component {
-  isFreshPint = true;
-  svgString = encodeURIComponent(renderToStaticMarkup(<PintBackground gamma={0} width={window.innerWidth} height={window.innerHeight} />));
+  svgString = encodeURIComponent(
+    renderToStaticMarkup(
+      <PintBackground
+        gamma={0}
+        width={window.innerWidth}
+        height={window.innerHeight}
+      />
+    )
+  );
   state = {
     gamma: 0,
-    dataUri: `url("data:image/svg+xml,${this.svgString}`,
+    poured: 0,
+    dataUri: `url("data:image/svg+xml,${this.svgString}`
   };
 
   componentDidMount() {
     if (window.DeviceOrientationEvent) {
-      console.log('device orientation is supported 😁');
-      let self = this;
-      window.addEventListener('deviceorientation', event => {
-        // console.log(event);
-        // console.log('typeof gamma: ', typeof event.gamma);
-        // const gamma = Math.round(Number(event.gamma));
-        // console.log('gamma: ', gamma);
-        self.setState({ ...self.state, gamma: event.gamma });
-      }, true);
+      console.log("device orientation is supported 😁");
+      window.addEventListener(
+        "deviceorientation",
+        event => {
+          this.setState({ gamma: event.gamma });
+        },
+        true
+      );
     }
-  };
+  }
 
   componentDidUpdate() {
-    this.isFreshPint = false;
-    const svgString = encodeURIComponent(renderToStaticMarkup(<PintBackground gamma={this.state.gamma} width={window.innerWidth} height={window.innerHeight} />));
-    this.isFreshPint = !this.isFreshPint;
-    // console.log('svg string: ', svgString);
+    if (
+      this.state.poured <
+      (Math.abs(this.state.gamma) / 90) * window.innerHeight
+    ) {
+      this.setState({
+        poured: (Math.abs(this.state.gamma) / 90) * window.innerHeight
+      });
+    }
+
+    const svgString = encodeURIComponent(
+      renderToStaticMarkup(
+        <PintBackground
+          poured={this.state.poured}
+          gamma={this.state.gamma}
+          width={window.innerWidth}
+          height={window.innerHeight}
+        />
+      )
+    );
     const dataUri = `url("data:image/svg+xml,${svgString}")`;
     this.state.dataUri !== dataUri && this.setState({ dataUri });
   }
 
   componentWillUnmount() {
-    window.removeEventListener('deviceorientation', this.handleOrientation, true);
+    window.removeEventListener("deviceorientation");
   }
 
   render() {
     return (
       <div className="App">
         <Helmet title="Pick a Pint" />
-        <div className="background" style={{ background: this.state.dataUri, backgroundPosition: 'bottom' }} >
+        <div
+          className="background"
+          style={{
+            background: this.state.dataUri
+          }}
+        >
           <div className="Content">
             <div className="App-header">
               {/* <img src={logo} className="App-logo" alt="logo" />
               <span role="img"  className="App-logo" aria-label="pint emoji">🍺</span> */}
-              <h1 className='App-title'>Pick a Pint!</h1>
+              <h1 className="App-title">Pick a Pint!</h1>
             </div>
             <div className="Body">
               <p className="Intro">
-                Currently in development so please, watch this space! {this.state.gamma}
+                Currently in development so please, watch this space!{" "}
+                {this.state.gamma}
               </p>
             </div>
             <div className="Footer">
-              <a href='https://www.linkedin.com/in/tommyhobbs/' target="_blank" rel="noopener noreferrer">
-                <img src={linkedIn} className="Logo LinkedIn" alt="LinkedIn Logo" />
+              <a
+                href="https://www.linkedin.com/in/tommyhobbs/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={linkedIn}
+                  className="Logo LinkedIn"
+                  alt="LinkedIn Logo"
+                />
               </a>
-              <a href='https://github.com/tommyhobbs/pick-a-pint' target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://github.com/tommyhobbs/pick-a-pint"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img src={gitHub} className="Logo GitHub" alt="GitHub Logo" />
               </a>
               <p>a silly idea by Tom Hobbs </p>
--- src/components/pintBackground/PintBackground.jsx
+++ src/components/pintBackground/PintBackground.jsx
@@ -1,37 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 // import PropTypes from 'prop-types';
 // import beer from '../../assets/beer.png';
 import "./pintBackground.css";
 
-class PintBackground extends Component {
-  render() {
-    const { gamma, width, height } = this.props;
-    const angle = Math.abs(gamma);
-    const xPos = gamma < 0 ? 0 : -width;
-    const flip = gamma < 0 ? 1 : -1;
-    return (
-      <svg xmlns="http://www.w3.org/2000/svg" width={width}>
-        <rect
-          width={width}
-          height={height}
-          style={{
-            fill: "#F3CD57",
-            transform: `scaleX(${flip}) translate(${xPos}px, ${0}px) skewY(${angle}deg)`
-          }}
-        />
-        <rect
-          width="10"
-          height="10"
-          style={{
-            stroke: "#006600",
-            fill: "#00cc00"
-          }}
-          x={xPos}
-          y={0}
-        />
-      </svg>
-    );
-  }
-}
+const PintBackground = props => {
+  const { gamma, width, height, poured } = props;
+  const angle = Math.abs(gamma);
+  const xPos = gamma < 0 ? 0 : -width;
+  const flip = gamma < 0 ? 1 : -1;
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={width}>
+      <rect
+        width={width}
+        height={height}
+        style={{
+          fill: "#F3CD57",
+          transform: `scaleX(${flip}) translate(${xPos}px, ${poured}px) skewY(${angle}deg)`
+        }}
+      />
+      <rect
+        width="10"
+        height="10"
+        style={{
+          stroke: "#006600",
+          fill: "#00cc00"
+        }}
+        x={xPos}
+        y={0}
+      />
+    </svg>
+  );
+};
 
 export default PintBackground;
